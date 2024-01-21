34eae8d42ae142549cb08fc76477d2e107cfb950
Nipu Das <nipuforwork3@gmail.com>
2024-01-21 23:11:16 +0530

    Initial code added

---
+++ index.js
@@ -0,0 +1,12 @@
+const age = 20; 
+const name = 'Nipu';
+const dob = '21-09-2004';
+
+function sayName() {
+    console.log('My name is' + name1);');
+}
+
+function isAbove18() {
+    return age >= 18;
+}
+
